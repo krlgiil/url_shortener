e5ea2a88b374aacbffcbb759a652433a60b590cd
Karl Lof <lof.karl@yahoo.fr>
2019-01-17 11:13:34 +0400

    feat: url-shortener - add db connection - add sequelize - add compose file to run mysql service - add dotenv - creation of new route /submit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+import Connection from './database/connection';
 
 const app = express();
 
 app.set('views', './src/views');
 app.set('view engine', 'pug');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+app.post('/submit', (req, res) => {
+  const { url } = req.body;
+  if (url) {
+    /** @todo insert url in db */
+  }
+
   res.render('homepage');
 });
 
